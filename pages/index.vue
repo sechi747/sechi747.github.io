a781f96201eacbd7f4fbd80f4f50a447039ed684
sechi <sechichen@gmail.com>
2022-10-21 22:27:39 +0800

    chore: clean useless files & add @nuxt/content

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,12 @@
 <template>
   <div>
-    <Logos mb-6 />
+    <DarkToggle />
     <Suspense>
-      <PageView />
       <template #fallback>
         <div op50 italic>
           <span animate-pulse>Loading...</span>
         </div>
       </template>
     </Suspense>
-    <InputEntry />
   </div>
 </template>
