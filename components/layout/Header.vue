68bfed250e609e978ec23f5b519f97d976f11872
sechi <sechichen@gmail.com>
2022-10-24 20:57:04 +0800

    feat: add header links

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -9,28 +9,34 @@ const logoUrl = computed(() => isDark.value ? '/logo-light.svg' : '/logo.svg')
     fbc
     bg="gray-300/20"
   >
-    <div icon-btn>
-      <img
-        h-6
-        left-8
-        top-6
-        :src="logoUrl"
-        alt="logo"
-      >
-    </div>
+    <NuxtLink to="/">
+      <div icon-btn>
+        <img
+          h-6
+          left-8
+          top-6
+          :src="logoUrl"
+          alt="logo"
+        >
+      </div>
+    </NuxtLink>
     <div grid gap-5 auto-flow-col items-center>
-      <i
-        class="i-carbon-blog icon-btn"
-        :class="isDark ? 'hover:text-white' : 'hover:text-emerald-500'"
-      />
+      <NuxtLink to="/posts">
+        <i
+          class="i-carbon-blog icon-btn"
+          :class="isDark ? 'hover:text-white' : 'hover:text-emerald-500'"
+        />
+      </NuxtLink>
       <i
         class="i-carbon:pen-fountain icon-btn"
         :class="isDark ? 'hover:text-white' : 'hover:text-emerald-500'"
       />
-      <i
-        class="i-carbon-logo-github icon-btn"
-        :class="isDark ? 'hover:text-white' : 'hover:text-black'"
-      />
+      <NuxtLink to="https://github.com/sechi747" target="_blank">
+        <i
+          class="i-carbon-logo-github icon-btn"
+          :class="isDark ? 'hover:text-white' : 'hover:text-black'"
+        />
+      </NuxtLink>
       <DarkToggle />
     </div>
   </div>
