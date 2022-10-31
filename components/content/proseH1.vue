b710266ef6eb3d797de32ea8d7001d835f98c486
sechi <sechichen@gmail.com>
2022-10-31 17:30:30 +0800

    feat: add proseH1.vue

---
+++ components/content/proseH1.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{ id: string }>()
+</script>
+
+<template>
+  <h1 :id="id" text-3xl class="group">
+    {{ id }}
+    <a header-anchor no-underline :href="`#${id}`">
+      #
+    </a>
+  </h1>
+</template>
