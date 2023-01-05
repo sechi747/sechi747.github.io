07e816f2074214ebf5327a39220de41ad6de8eb5
sechi <sechichen@gmail.com>
2023-01-05 17:06:47 +0800

    feat: add steam info

--- pages/index.vue
+++ pages/index.vue
@@ -51,5 +51,8 @@ useTitle('PlantSechi')
     <p>✨ Focusing on Vue related ecology.</p>
     <p>🧐 I'm also learning TypeScript, Node.js and React recently. </p>
     <p>👾 I'm currently interested in Overwatch2 (but it will gone soon😅). </p>
+    <p>🎮 Welcome to add my steam friends! My SteamID is 76561198863705753</p>
+
+    <img width="450" height="170" src="https://card.yuy1n.io/card/76561198863705753/tokyonight,en,badge,text-9ea1a5,bg-2d3034">
   </div>
 </template>
