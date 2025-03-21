6e12551df3f162a0235d9959f81a82db9b316d6e
sechi <sechichen@gmail.com>
2025-03-21 17:52:11 +0800

    chore: update bio

--- pages/index.vue
+++ pages/index.vue
@@ -49,7 +49,6 @@ useHead({ title: 'PlantSechi' })
     <p>✨ Focusing on Vue related ecology.</p>
     <p>🧐 I'm also learning TypeScript, Node.js and React recently. </p>
     <p>🎮 Welcome to add my steam friends! My SteamID is 76561198863705753</p>
-    <p>🎹 Currently learning piano by myself.</p>
 
     <img width="450" height="170" alt="steam_card" src="https://card.yuy1n.io/card/76561198863705753/tokyonight,en,badge,text-9ea1a5,bg-2d3034">
   </div>
